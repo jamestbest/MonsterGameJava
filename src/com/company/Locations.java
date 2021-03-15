5db77d68539a7d801254812d5582312acbf49f66
James <james2004.coward@gmail.com>
2021-03-15 18:38:13 +0000

    test

--- src/com/company/Locations.java
+++ src/com/company/Locations.java
@@ -1,4 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Locations {
+    int x;
+    int y;
+    boolean showOb = false;
+    String name;
+
+    public void moveX(int moveXModifier){
+        this.x += moveXModifier;
+        System.out.println(this.x);
+    }
+
+    public void moveY(int moveYModifier){
+        this.y += moveYModifier;
+        System.out.println(this.y);
+    }
+
+    public void isShow(ArrayList<ArrayList<String>> TwoD){
+        if (showOb){
+            TwoD.get(this.x).set(this.y,this.name);
+        }
+    }
+
+    public void setlocatio(ArrayList<ArrayList<String>> TwoD){
+        TwoD.get(this.x).set(this.y, this.name);
+    }
+
+    public void removeLocation(ArrayList<ArrayList<String>> TwoD){
+        TwoD.get(this.x).set(this.y, "X");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,187 @@
 package com.company;
 
+import javax.sound.sampled.AudioInputStream;
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class Main {
 
+
+    private static Locations monster = new Locations();
+    private static Locations flask = new Locations();
+    private static Locations trapOne = new Locations();
+    private static Locations trapTwo = new Locations();
+    private static Locations player = new Locations();
+
+    private static int length = 10;
+    private static int height = 10;
+    private static ArrayList<ArrayList<String>> TwoD = initialiseTwoDArray(length, height);
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the monster game!");
+        System.out.println("Welcome to the monster game");
 
-        // create a 2d array to store the map with 'x' for area and 'M'.'P'
-        // create a 3d array to store the co-ordinates so they can be selected by each object
-        // create an object array to store the x,y and moving methods
+        int length = 10;
+        int height = 10;
 
-        int height = 5;
-        int width = 5;
+        ArrayList<ArrayList<ArrayList<Integer>>> ThreeD = initialiseThreeDArray(length, height);
 
 
-        ArrayList<ArrayList<String>> TwoDMap = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<ArrayList<Integer>>> ThreeDMap = new ArrayList<ArrayList<ArrayList<Integer>>>(height);
+        int[] temp = Createlocation(ThreeD);
+        monster.x = temp[0];
+        monster.y = temp[1];
+        monster.name = "M";
 
-        for (int i = 0; i < height; i++) {
-            ThreeDMap.add(new ArrayList<>(height));
-            for (int j = 0; j < width; j++) {
-                ThreeDMap.get(i).add(new ArrayList<>(width));
-                ThreeDMap.get(i).get(j).add(i);
-                ThreeDMap.get(i).get(j).add(j);
-            }
+        temp = Createlocation(ThreeD);
+        flask.x = temp[0];
+        flask.y = temp[1];
+        flask.name = "F";
+
+        temp = Createlocation(ThreeD);
+        trapOne.x = temp[0];
+        trapOne.y = temp[1];
+        trapOne.name = "T";
+
+        temp = Createlocation(ThreeD);
+        trapTwo.x = temp[0];
+        trapTwo.y = temp[1];
+        trapTwo.name = "T";
+
+        temp = Createlocation(ThreeD);
+        player.x = temp[0];
+        player.y = temp[1];
+        player.name = "P";
+
+
+        boolean end = false;
+        player.showOb = true;
+
+        player.isShow(TwoD);
+        flask.isShow(TwoD);
+        monster.isShow(TwoD);
+        trapOne.isShow(TwoD);
+        trapTwo.isShow(TwoD);
+
+        while (!end) {
+            showTwoDRows(TwoD);
+            end = Choice();
+
+
         }
 
 
-        System.out.println(ThreeDMap.get(2).get(3));
-        System.out.println(ThreeDMap.get(4).get(1));
+    }
 
+    public static int[] Createlocation(ArrayList<ArrayList<ArrayList<Integer>>> ThreeD) {
+        Random random = new Random();
+        int x = random.nextInt(ThreeD.size());
+        int y = random.nextInt(ThreeD.get(x).size());
+
+        int[] coords;
+        coords = new int[]{ThreeD.get(x).get(y).get(0), ThreeD.get(x).get(y).get(1)};
+        ThreeD.get(x).remove(y);
+
+
+        return coords;
+    }
+
+    public static void showThreeDIndividual(ArrayList<ArrayList<ArrayList<Integer>>> ThreeD) {
+        for (int i = 0; i < ThreeD.size(); i++) {
+            for (int j = 0; j < ThreeD.get(i).size(); j++) {
+                System.out.println(ThreeD.get(i).get(j));
+            }
+        }
+    }
+
+    public static void showThreeDRows(ArrayList<ArrayList<ArrayList<Integer>>> ThreeD) {
+        for (int i = 0; i < ThreeD.size(); i++) {
+            System.out.println(ThreeD.get(i));
+        }
+    }
+
+
+    public static ArrayList<ArrayList<ArrayList<Integer>>> initialiseThreeDArray(int length, int height) {
+        ArrayList<ArrayList<ArrayList<Integer>>> ThreeD = new ArrayList<>(length);
+
+        for (int i = 0; i < length; i++) {
+            ThreeD.add(new ArrayList<>(length));
+            for (int j = 0; j < height; j++) {
+                ThreeD.get(i).add(new ArrayList<>(height));
+                ThreeD.get(i).get(j).add(i);
+                ThreeD.get(i).get(j).add(j);
+            }
+        }
+
+        return ThreeD;
 
     }
+
+    public static ArrayList<ArrayList<String>> initialiseTwoDArray(int length, int height) {
+        ArrayList<ArrayList<String>> TwoD = new ArrayList<>(length);
+        for (int i = 0; i < length; i++) {
+            TwoD.add(new ArrayList<>(length));
+            for (int j = 0; j < height; j++) {
+                TwoD.get(i).add("X");
+            }
+
+
+        }
+
+        return TwoD;
+    }
+
+    public static void showTwoDRows(ArrayList<ArrayList<String>> TwoD) {
+        for (int i = 0; i < TwoD.size(); i++) {
+            System.out.println(TwoD.get(i));
+        }
+    }
+
+    public static boolean Choice() {
+        System.out.println("Please enter WASD to move or F to exit the program");
+        Scanner sc = new Scanner(System.in);
+        String choice = sc.next();
+
+        if (choice.toLowerCase().equals("f")) {
+            return true;
+        } else if ((choice.equalsIgnoreCase("w")) || (choice.equalsIgnoreCase("a")) || (choice.equalsIgnoreCase("s")) || (choice.equalsIgnoreCase("d"))) {
+            movingPlayer(choice.toLowerCase());
+            return false;
+        } else {
+            System.out.println("invalid input");
+            Choice();
+        }
+        return false;
+    }
+
+    public static void movingPlayer(String movement) {
+        player.removeLocation(TwoD);
+        if (movement.equals("w")) {
+            player.moveX(-1);
+        }
+        else if (movement.equals("d")){
+            player.moveY(1);
+        }
+        else if(movement.equals("s")){
+            player.moveX(1);
+        }
+        else if (movement.equals("a")){
+            player.moveY(-1);
+        }
+        player.setlocatio(TwoD);
+    }
+
+
 }
